ef1f25ba6d353cf52528f1f5594cc625bf5a27e1
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-03-16 19:55:01 +0100

    MVC alap megcsinálva
    Modell View Control

--- beadando/routes/web.php
+++ beadando/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//ide kellenek az url-ek
+Route::get('/test',[TestController::class, 'index']); //első rész a kontroller, a másik a függvény a TestController classban
+

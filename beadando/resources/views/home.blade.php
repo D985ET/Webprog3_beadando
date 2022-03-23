b1c456f0f16dc9547dde440318a39b8c412bd0c3
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-03-23 19:22:25 +0100

    probálkozás kiiratásra

--- beadando/resources/views/home.blade.php
+++ beadando/resources/views/home.blade.php
@@ -3,8 +3,25 @@
 @section('content')
 <div class="row">
 <div class="col-md8 col-lg-6 mx-auto">
-    @foreach($posts as $post)
-        @include('post._items');
+    @foreach($topic as $topics)
+    <div class="card mb-3">
+        <div class="card-body">
+            <div class="d-flex">
+                <img width="150" height="150" src="https://via.placeholder.com/350" alt=" {{$topics->title}}">
+                <div class="ms-3">
+                    {{$topics->topic->title}}
+                </div>
+                <p class="fw-bold">
+                    {{$topics->description}}
+                </p>
+                <p class="text-end">
+                    <a href="{{route('post.details',$topics)}}" class="btn btn-sm btn-primary">
+                        Read more
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
     @endforeach
 </div>
 </div>

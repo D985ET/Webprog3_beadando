3659724e8836410bf6dc47f0e76715bc5e4fb607
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-03-24 17:39:04 +0100

    Revert "Controllerrek"
    This reverts commit 180e7c704ab3a18e8b7b788fbf05f7a41d620ce3.

--- beadando/app/Http/Controllers/TestController.php
+++ beadando/app/Http/Controllers/TestController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class TestController extends Controller
 {
     public function index()
     {
-        return view('home');//->with(compact('posts'));
+        return view('test');
     }
 }
--- beadando/routes/web.php
+++ beadando/routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\TestController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 //ide kellenek az url-ek
 Route::get('/test',[TestController::class, 'index']); //első rész a kontroller, a másik a függvény a TestController classban

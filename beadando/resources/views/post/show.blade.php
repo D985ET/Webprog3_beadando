b0c659e93c6f5e2527b2e320ab3f5de2bcf59345
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-04-06 09:57:45 +0200

    border hozzáadva

--- beadando/resources/views/post/show.blade.php
+++ beadando/resources/views/post/show.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-<h1 class="display-1">{{$post->title}}</h1>
-<p>{{$post->author->name}} | {{$post->topic->title}} | {{__('Maximum: ')}}{{$post->maxPlayer}} {{__('fő számára')}} | {{$post->created_at->diffForHumans()}}</p>
+<div class="d-flex text-center ">
+    <img class="card-img-left " width="300" height="300" src="{{$post->cover_image }}" alt="{{ $post->title }}" > 
+
+    <b><h1 class="display-1 mt-4" >{{$post->title}}</h1></b>
+</div>
+<div class="border-top border-5 mb-2">
+    
+</div>
+<p> {{$post->topic->title}} | {{__('Maximum: ')}}{{$post->maxPlayer}} {{__('fő számára')}} | {{$post->created_at->diffForHumans()}}</p>
 <p class="fw-bold">{{$post->description}}</p>
 
 <div>
     <h5>{{__('Játék leírás:')}}</h5>
     {!!$post->content!!} <!--rendereli a html-t-->
 </div>
+
 @endsection
